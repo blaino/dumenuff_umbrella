0ec5200fa86549a40b5b29a07a4d7b4e106123c4
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-12-01 07:27:15 -0500

    mix calls npm install for rivescript

--- apps/dumenuff_bots/mix.exs
+++ apps/dumenuff_bots/mix.exs
@@ -1,36 +1,28 @@
+defmodule Mix.Tasks.NpmInstall do
+  use Mix.Task
+
+  def run(_) do
+    Mix.shell().info("Running npm install to get rivescript and its dependencies")
+    Mix.shell().cmd("npm install")
+  end
+end
+
 defmodule DumenuffBots.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :dumenuff_bots,
+      default_task: "NpmInstall",
       version: "0.1.0",
       elixir: "~> 1.9",
-      start_permanent: Mix.env() == :prod,
+      # start_permanent: Mix.env() == :prod,
       # deps: deps(),
       # Umbrella
       build_path: "../../_build",
       config_path: "../../config/config.exs",
-      deps_path: "../../deps",
       lockfile: "../../mix.lock",
+      aliases: aliases()
     ]
   end
-
-  # # Run "mix help compile.app" to learn about applications.
-  # def application do
-  #   [
-  #     extra_applications: [:logger],
-  #     mod: {DumenuffEngine.Application, []}
-  #   ]
-  # end
-
-  # # Run "mix help deps" to learn about dependencies.
-  # defp deps do
-  #   [
-  #     {:phoenix_pubsub, "~> 1.1"},
-  #     {:uuid, "~> 1.1"}
-  #     # {:dep_from_hexpm, "~> 0.3.0"},
-  #     # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #   ]
-  # end
 end
