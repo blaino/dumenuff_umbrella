04de11ed531fe128fa87358ba338fb16c96af3fd
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-12-07 08:06:05 -0500

    Comment out pre-install hook

--- rel/hooks/pre_start/npm_install.sh
+++ rel/hooks/pre_start/npm_install.sh
@@ -1,3 +1,4 @@
 pwd
-cd ../../../../_build/prod/rel/dumenuff_umbrella/lib/dumenuff_bots-0.1.0/priv/node
-npm install
+echo "This is where you tried to use a pre-install hook"
+#cd ../../../../_build/prod/rel/dumenuff_umbrella/lib/dumenuff_bots-0.1.0/priv/node
+#npm install
