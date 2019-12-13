a8f07c47f8e9ba4efe4d7cb4c39eb5c8ef503e4c
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-12-13 17:09:21 -0500

    a start on sorting by scores

--- apps/dumenuff_interface/lib/dumenuff_interface_web/views/scores_view.ex
+++ apps/dumenuff_interface/lib/dumenuff_interface_web/views/scores_view.ex
@@ -10,4 +10,18 @@ defmodule DumenuffInterfaceWeb.ScoresView do
   def real_name(%Player{ethnicity: :human} = player) do
     player.real_name
   end
+
+  def score(%Player{scores: scores} = player) do
+    IO.inspect(scores, label: "scores")
+    4
+  end
+
+  def sort_by_score(players) do
+    Enum.sort_by(players, sum_scores(Map.fetch(&1, :scores)))
+  end
+
+  def sum_scores(scores) do
+    IO.inspect(scores, label: "sum_scores / scores")
+    Enum.reduce(scores, 0, fn({_k, v}, acc) -> v + acc end)
+  end
 end
