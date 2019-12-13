a4cc85459090fea5d2846cfecf1249ad01594698
Blaine Nelson <blaine.m.nelson@gmail.com>
2019-12-13 16:47:44 -0500

    real names in score page

--- apps/dumenuff_interface/lib/dumenuff_interface_web/views/scores_view.ex
+++ apps/dumenuff_interface/lib/dumenuff_interface_web/views/scores_view.ex
@@ -1,3 +1,13 @@
 defmodule DumenuffInterfaceWeb.ScoresView do
   use DumenuffInterfaceWeb, :view
+
+  alias DumenuffEngine.{Player}
+
+  def real_name(%Player{ethnicity: :bot} = player) do
+    "a bot"
+  end
+
+  def real_name(%Player{ethnicity: :human} = player) do
+    player.real_name
+  end
 end
